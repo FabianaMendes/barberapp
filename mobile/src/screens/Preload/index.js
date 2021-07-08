331a6b3e0694d1cd8c1a31a9ff224c343408386e
unknown <fabiana.mendes@oniweb.com.br>
2021-07-08 16:18:09 -0300

    Preload, SignIn & SignUp screens + api & userContext

--- mobile/src/screens/Preload/index.js
+++ mobile/src/screens/Preload/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Container, LoadingIcon } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import Api from '../../Api';
+import { UserContext } from '../../contexts/UserContext';
 
 import BarberLogo from '../../assets/barber.svg';
 
 export default () => {
+    const { dispatch: userDispatch } = useContext(UserContext);
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token');
+            if(token) {
+                let res = await Api.checkToken(token);
+                if(res.token) {
+                    await AsyncStorage.setItem('token', res.token);
+
+                    userDispatch({
+                        type: 'setAvatar',
+                        payload: {
+                            avatar: res.data.avatar
+                        }
+                    });
+
+                    navigation.reset({
+                        routes: [{ name:'MainTab' }]
+                    });
+
+                } else {
+                    navigation.navigate('SignIn');
+                }
+                
+            } else {
+                navigation.navigate('SignIn');
+            }
+        }
+        checkToken();
+    }, []);
+
+
     return (
         <Container>
             <BarberLogo width="100%" height="160"/>
